561292ef18a931433c0a91ce9d00ac3290e93dab
thanhha181094 <thanhha181094@gmail.com>
2019-03-29 14:51:28 +0700

    Add get api

--- database/migrations/2019_03_28_095539_create_articles_table.php
+++ database/migrations/2019_03_28_095539_create_articles_table.php
@@ -15,8 +15,8 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
             $table->string("content");
+            $table->timestamps();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('articles', 'ArticleController@index');
+//Route::get('articles/{id}', 'ArticleController@show');
+//Route::post('articles', 'ArticleController@store');
+//Route::put('articles/{id}', 'ArticleController@update');
+//Route::delete('articles/{id}', 'ArticleController@delete');
